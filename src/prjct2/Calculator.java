46df09a8d27885f223e121729c0478ed3d448332
VEERAMADHAVI <VEERAMADHAVI@DESKTOP-F352LSS>
2022-02-20 18:07:00 +0530

    commiting

---
+++ src/prjct2/Calculator.java
@@ -0,0 +1,18 @@
+package prjct2;
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i=8;
+		int j= 3;
+		System.out.println("Addition:"+(i+j));
+		System.out.println("Subtraction:"+(i-j));
+		System.out.println("Division:"+(i/j));
+		System.out.println("Mod:"+(i%j));
+			
+		}
+
+	}
+ 
+
